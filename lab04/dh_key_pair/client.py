568b763226be287cb95592f0b4d0702fb16becdd
kimtrong <trongtran2015blue@gmail.com>
2025-03-18 14:48:23 +0700

    lab04

--- lab04/dh_key_pair/client.py
+++ lab04/dh_key_pair/client.py
@@ -1,6 +1,7 @@
 from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.asymmetric import dh
 from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 
 def generate_client_key_pair(parameters):
     private_key = parameters.generate_private_key()
@@ -22,15 +23,9 @@ def main():
     # Generate client's key pair
     private_key, public_key = generate_client_key_pair(parameters)
 
-    # Save client's public key
-    with open("client_public_key.pem", "wb") as f:
-        f.write(public_key.public_bytes(
-            encoding=serialization.Encoding.PEM,
-            format=serialization.PublicFormat.SubjectPublicKeyInfo
-        ))
-
     # Derive shared secret
     shared_secret = derive_shared_secret(private_key, server_public_key)
+    print("Shared secret:", shared_secret)
 
 if __name__ == "__main__":
     main()
